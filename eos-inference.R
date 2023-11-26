a82316a6539896d5b295812921db1857851ce3c4
sakulcodes <smahat@tamu.edu>
2023-11-26 16:28:18 -0600

    new updates

--- eos-inference.R
+++ eos-inference.R
@@ -14,62 +14,59 @@ miller_data <- read.table("/Users/sakul/Desktop/RESEARCH/PRL_rev/miller/J0030_3s
 combined_data <- rbind(na.omit(setNames(riley_data, c("R", "M"))), setNames(miller_data, c("R", "M")))
 observed_M <- combined_data$M ; observed_R <- combined_data$R
 correlation <- cor(observed_M, observed_R); print(correlation)
-plot(observed_R, observed_M, xlab = "Observed Radius", ylab = "Observed Mass")
-obs_cov_mat <- cov(combined_data) #covariance matrix
+plot(observed_R, observed_M, xlab = "Observed Radius", ylab = "Observed Mass", main ="Observed Data")
+obs_cov_mat <- cov(combined_data) ; obs_cov_mat#covariance matrix
 
 #generate data:
 n = length(observed_M)
 correlated_noise = mvrnorm(n, mu = c(0, 0), Sigma = obs_cov_mat)
-m_observed = observed_M + correlated_noise[,1] ; r_observed = observed_R + correlated_noise[,2]
-plot(m_observed, r_observed)
+m_observed = observed_M + correlated_noise[,2] ; r_observed = observed_R + correlated_noise[,1]
+plot(r_observed, m_observed, main ="Generated Data")
 
-#model 
+#Bayesian Parametric Model 
 # (m_i, r_i)' | p_i, theta, Sigma  ~ N_2( {m(p_i, theta),r(p_i, theta)} , Sigma) 
 #              theta  ~ Discrete Uniform(theta_1,....,theta_K) 
-#               p_i | theta ~ Discrete Uniform {Pmin(theta),....,Pmax(theta)} independetly for i = 1 to n
+#               p_i | theta ~ Discrete Uniform {Pmin(theta),....,Pmax(theta)} independently for i = 1 to n
 #               Sigma ~ Inverse_Wishart(nu = 2, Psi = diag(2))
+
+
+# ------------------------ ORACLE MATRIX  - {M(pi,theta), R(pi,theta)} - Implicit Relationship - Solves the EOS & TOV:
+#The FORTRAN oracle should solve (M(pi,theta), R(pi, theta)) FOR EVERY COMBINATION in the grid values for theta and p. 
+#Imagine a N by K matrix, N = no. of central densities/central pressure, K = no.of theta 
+
+#loading the oracle matrix:
+max_rows <- 0 ; num_files <- 100 ; base_path <- "/Users/sakul/Desktop/ldmcrust/res_eos_pnm32/eft_pnm32_"
+M_Oracle <- R_Oracle <- list() #Initialize with NAs
+
+for (i in 1:num_files) {
+  file_name <- sprintf("%s%06d_ldm_nsmr.dat", base_path, i) 
+  data_eos <- read.table(file_name)
+  max_rows <- max(max_rows, nrow(data_eos))
+  M_Oracle[[i]] <- data_eos$V2
+  R_Oracle[[i]] <- data_eos$V3
+}
+M_Oracle <- do.call(cbind, lapply(M_Oracle, function(x) { length(x) <- max_rows; x }))
+R_Oracle <- do.call(cbind, lapply(R_Oracle, function(x) { length(x) <- max_rows; x }))
+
+# -----> Parametric curve visualization 
+plot( R_Oracle[,1], M_Oracle[,1], xlab = "Radius", ylab = "Mass", col = "blue") ; points(R_Oracle[,2], M_Oracle[,2], col = "red")
+points(R_Oracle[,3], M_Oracle[,3], col = "green")  
+
+na_count_M <- sum(is.na(M_Oracle)); na_count_R <- sum(is.na(R_Oracle)) #NA sanity checks - Total Na values across the Oracle Matrix
+dim(M_Oracle) ; M_Oracle[300, 2] #example NA point - use indicator function to account for this.
+
 
 #grid for theta 
 prior <- read.table("/Users/sakul/Desktop/ldmcrust/data/eft_pnm32_kfr16_par_all.dat")[, -c(5, 6)]
-grid_theta <- prior ; grid_theta <- grid_theta[1:100,]  #for psuedo purposes, sampling only first 100 values as orcale also has only data points.
+grid_theta <- prior ; grid_theta <- grid_theta[1:100,]  
+#for pseudo purposees - taking grid for only the first 100 values as only 100 data points are loaded in the oracle matrix. 
 log_prior_theta = -log(nrow(grid_theta))
 
-# ------------------------ {M(pi,theta), R(pi,theta)} - implicit relationship - RESULT MATRIX
-#The FORTRAN oracle should solve (M(pi,theta), R(pi, theta)) FOR EVERY COMBINATION in the grid values for theta and p
 
-#Creating an oracle matrix of dimensions = k (number of theta) * n(number of central densities / central pressure) for every combination in the grid. 
-max_rows <- 0 ; num_files <- 100 ; base_path <- "/Users/sakul/Desktop/ldmcrust/res_eos_pnm32/eft_pnm32_"
-for (i in 1:num_files) {
-  file_name <- sprintf("%s%06d_ldm_nsmr.dat", base_path, i)
-  data_eos <- read.table(file_name)
-  max_rows <- max(max_rows, nrow(data_eos))
-}
-M_values <- matrix(NA, nrow = max_rows, ncol = num_files) ; R_values <- matrix(NA, nrow = max_rows, ncol = num_files)
+#WORK ON THIS - IS THE CENTRAL DENSITY REALLY THE FIRST COLUMN? - uses the "/Users/sakul/Desktop/ldmcrust/res_eos_pnm32/eft_pnm32_"
+#I DO NOT THINK THIS IS DONE CORRECTLY - THINK ABOUT IT.
 
-# Fill the matrices
-for (i in 1:num_files) {
-  file_name <- sprintf("%s%06d_ldm_nsmr.dat", base_path, i)
-  data_eos <- read.table(file_name)
-  M_values[1:nrow(data_eos), i] <- data_eos$V2
-  R_values[1:nrow(data_eos), i] <- data_eos$V3
-}
-
-M_values; R_values #oracle matrix formulation, each row corresponds to a central density and each column is the solution of a theta value. 
-dim(M_values) ; M_values[300, 2] #should give NA
-#Note - FORTRAN EOS solution needs to be extended if we decide to extend the grid. 
-
-#Replacing the NAs values in every column with the mean of Non-NA values in that column (mean imputation) - just for now, will think about this later.
-M_values <- apply(M_values, 2, function(col) {
-  col[is.na(col)] <- mean(col, na.rm = TRUE)
-  return(col)
-})
-
-R_values <- apply(R_values, 2, function(col) {
-  col[is.na(col)] <- mean(col, na.rm = TRUE)
-  return(col)
-})
-
-#grid for central density / central pressure
+#grid for central density (nc)
 all_densities <- c()
 for (i in 1:num_files) {
   file_name <- sprintf("%s%06d_ldm_nsmr.dat", base_path, i)
@@ -80,47 +77,61 @@ unique_densities <- sort(unique(all_densities)) ; unique_densities
 grid_p <- unique_densities ; log_prior_p = -log(length(grid_p))
 
 
-# -------------------------------Gibbs-Sampling-Setup:
+
+# ------------------------------Gibbs-Sampling-Setup:
 nreps = 500; nu = 2; Psi = diag(2) ; n = length(observed_M)
 #storage variables:
 theta_store = matrix(0, nrow = nreps, ncol = 8) ; p_store = matrix(0, nrow = nreps, ncol = n) ; Sigma_store = array(0, dim = c(2, 2, nreps))
-p = sample(grid_p, n, replace = TRUE) ; Sigma = rinvwishart(nu, Psi)  #initialization   
+#Initialize theta (take the row closest to the mean)
+theta_grid <- as.matrix(grid_theta)
+mean = colMeans(theta_grid) ; distances = apply(theta_grid, 1, function(row) sum((row - mean)^2))  # Calculate squared distances
+theta = theta_grid[which.min(distances), ]
+
+#Initialize Sigma:
+Sigma = rinvwishart(nu, Psi)  #initialization
 
 for (iter in 1:nreps) {
   iSigma = solve(Sigma)
-  # Update theta vector
-  theta_prob_log = sapply(1:nrow(grid_theta), function(i) {  # Loop over columns (theta values)
-    expected_m = M_values[, i] ; expected_r = R_values[, i]
-    res = rbind(m_observed - expected_m[p], r_observed - expected_r[p])
+  
+  #Update p
+  theta_idx = which(apply(theta_grid, 1, function(row) all(row == theta)))
+  for (i in 1:n) {
+    p_prob_log = mapply(function(p_val) {
+      p_index = which(grid_p == p_val) 
+      indicator = ifelse(is.na(M_Oracle[p_index, theta_idx]), 0, 1)  # Indicator function.
+      if (indicator == 0) {
+        return(0)
+      } else {
+        expected_m = M_Oracle[p_index, theta_idx]
+        expected_r = R_Oracle[p_index, theta_idx]
+        res = c(m_observed[i] - expected_m, r_observed[i] - expected_r)
+        return(-0.5 * res %*% iSigma %*% res)
+      }
+    }, grid_p)
+    unnorm_prob = exp(p_prob_log - max(p_prob_log))
+    norm_prob = unnorm_prob / sum(unnorm_prob)
+    p_store[iter, i] = sample(theta_grid, 1, prob = norm_prob)
+  }
+  p = p_store[iter,] #sampled pressure
+  sampled_p_indexes = match(p, grid_p)
+  
+  #Update theta vector
+  theta_prob_log = sapply(1:ncol(theta_grid), function(i) {  # Loop over columns (theta values)
+    expected_m = M_Oracle[, i] ; expected_r = R_Oracle[, i]
+    res = rbind(m_observed - expected_m[sampled_p_indexes], r_observed - expected_r[sampled_p_indexes])
     -0.5 * sum(diag(t(res) %*% iSigma %*% res))
   })
   unnorm_prob = exp(theta_prob_log - max(theta_prob_log))
   norm_prob = unnorm_prob / sum(unnorm_prob)
-  sampled_row_idx = sample(1:nrow(grid_theta), 1, prob = norm_prob)
-  sampled_theta = grid_theta[sampled_row_idx, ]
+  sampled_row_idx = sample(1:nrow(theta_grid), 1, prob = norm_prob)
+  sampled_theta = theta_grid[sampled_row_idx, ]
   theta_store[iter, ] = sampled_theta
-  
-  # Update p
-  theta_idx = which(apply(grid_theta, 1, function(row) all(row == theta)))
-  
-  for (i in 1:n)
-  {
-    p_prob_log = mapply(function(p_val) {
-      # Get the expected m and r values for the current p and previously sampled theta
-      expected_m = M_values[p_val, theta_idx]
-      expected_r = R_values[p_val, theta_idx]
-      res = c(m_observed[i] - expected_m, r_observed[i] - expected_r)
-      -0.5 * res %*% iSigma %*% res + log_prior_p
-    }, grid_p)
-    unnorm_prob = exp(p_prob_log - max(p_prob_log))
-    norm_prob = unnorm_prob / sum(unnorm_prob)
-    p_store[iter, i] = sample(grid_p, 1, prob = norm_prob)
-  }
-  p = p_store[iter,]
   
   #update sigma 
   #find residuals based on previously sampled theta and p
-  residuals_m = m_observed - M_values[p, theta_idx] ; residuals_r = r_observed - R_values[p, theta_idx] 
+  theta_idx = which(apply(theta_grid, 1, function(row) all(row == sampled_theta)))
+  p_index = which(grid_p == p_val)
+  residuals_m = m_observed - M_Oracle[p_index, theta_idx] ; residuals_r = r_observed - R_Oracle[p_index, theta_idx] 
   res = rbind(residuals_m, residuals_r)
   S = res %*% t(res)
   # Updated the parameters for inverse wishart
@@ -138,7 +149,5 @@ for (i in 1:8) {
 }
 
 #Do some more goodness of fit checks:
-
-
 
 
